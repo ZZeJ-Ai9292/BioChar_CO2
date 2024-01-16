0ab0eec69f176b8bbf762a8086a235b5fc262e2c
Andirod9262 <155721755+Andirod9262@users.noreply.github.com>
2024-01-16 16:51:58 +0800

    Update App.py

--- App.py
+++ App.py
@@ -22,8 +22,8 @@ with col1:
 
 with col2:
     st.markdown('<p style="font-size: 24px; color: red; font-weight: bold; font-family: \'Times New Roman\', Times, serif; ">Pyrolysis</p>', unsafe_allow_html=True)
-    feature26 = st.slider('Wash-Pre (HCl/none/H<sub>2</sub>O —— −1/0/1)', min_value=-1, max_value=1, step=1, value=0)
-    feature27 = st.slider('Wash-Post (HCl/none/H<sub>2</sub>O —— −1/0/1)', min_value=-1, max_value=1, step=1, value=-1)
+    feature26 = st.slider('Wash-Pre (HCl/none/H2O —— −1/0/1)', min_value=-1, max_value=1, step=1, value=0)
+    feature27 = st.slider('Wash-Post (HCl/none/H2O —— −1/0/1)', min_value=-1, max_value=1, step=1, value=-1)
     feature28 = st.slider('HR (°C/min)', min_value=0, max_value=50, step=1, value=3)
     feature29 = st.slider('PT (°C)', min_value=0, max_value=1000, step=1, value=440)
     feature30 = st.slider('Pt (min)', min_value=0, max_value=240, step=1, value=50)
@@ -35,7 +35,7 @@ with col3:
         feature10, feature11, feature12, feature13, feature14, feature15 = 0, 0, 0, 0, 0, 0
         st.write(f'Without impregnation activation')
     elif feature9 == -1:
-        feature10 = st.slider('Imp-Agent (pK<sub>B</sub>)', min_value=-1.10, max_value=20.00, step=0.01, value=-0.56)
+        feature10 = st.slider('Imp-Agent (pKB)', min_value=-1.10, max_value=20.00, step=0.01, value=-0.56)
         feature11 = st.slider('Imp-t (min)', min_value=0, max_value=1440, step=1, value=530)
         feature12 = st.slider('Imp-T (°C)', min_value=0, max_value=80, step=1, value=24)
         feature13 = st.slider('Imp-Ratio', min_value=0.0, max_value=3.0, step=0.1, value=3.0)
@@ -44,7 +44,7 @@ with col3:
         st.write(f'Imp-AT (°C): {feature14}')
         st.write(f'Imp-At (min): {feature15}')
     else:
-        feature10 = st.slider('Imp-Agent (pK<sub>B</sub>)', min_value=-1.10, max_value=20.00, step=0.01, value=-0.56)
+        feature10 = st.slider('Imp-Agent (pKB)', min_value=-1.10, max_value=20.00, step=0.01, value=-0.56)
         feature11 = st.slider('Imp-t (min)', min_value=0, max_value=1440, step=1, value=530)
         feature12 = st.slider('Imp-T (°C)', min_value=0, max_value=80, step=1, value=24)
         feature13 = st.slider('Imp-Ratio', min_value=0.0, max_value=3.0, step=0.1, value=3.0)
@@ -58,14 +58,14 @@ with col4:
         feature17, feature18, feature19, feature20 = 0, 0, 0, 0
         st.write(f'Without mixing activation')
     elif feature16 == -1:
-        feature17 = st.slider('Mix-Agent (pK<sub>B</sub>)', min_value=-1.10, max_value=11.30, step=0.01, value=0.00)
+        feature17 = st.slider('Mix-Agent (pKB)', min_value=-1.10, max_value=11.30, step=0.01, value=0.00)
         feature18 = st.slider('Mix-Ratio', min_value=0.0, max_value=5.0, step=0.1, value=0.0)
         feature19 = feature29
         feature20 = feature30
         st.write(f'Mix-AT (°C): {feature19}')
         st.write(f'Mix-At (min): {feature20}')
     else:
-        feature17 = st.slider('Mix-Agent (pK<sub>B</sub>)', min_value=-1.10, max_value=11.30, step=0.01, value=0.00)
+        feature17 = st.slider('Mix-Agent (pKB)', min_value=-1.10, max_value=11.30, step=0.01, value=0.00)
         feature18 = st.slider('Mix-Ratio', min_value=0.0, max_value=5.0, step=0.1, value=0.0)
         feature19 = st.slider('Mix-AT (°C)', min_value=0, max_value=1000, step=10, value=0)
         feature20 = st.slider('Mix-At (min)', min_value=0, max_value=180, step=1, value=0)
@@ -77,22 +77,22 @@ with col5:
         feature22, feature23, feature24, feature25 = 0, 0, 0, 0
         st.write(f'Without physical activation')
     elif feature21 == -1:
-        feature22 = st.slider('Gas-Agent (steam/CO<sub>2</sub>/None(−1/1/0))', min_value=-1, max_value=1, step=1, value=0)
+        feature22 = st.slider('Gas-Agent (steam/CO2/None(−1/1/0))', min_value=-1, max_value=1, step=1, value=0)
         feature23 = st.slider('Gas-Flow (mL/min)', min_value=0.0, max_value=100.0, step=0.1, value=0.0)
         feature24 = feature29
         feature25 = feature30
         st.write(f'Gas-AT (°C): {feature24}') 
         st.write(f'Gas-At (min): {feature25}')
     else:
-        feature22 = st.slider('Gas-Agent (steam/none/CO<sub>2</sub> —— -1/0/1)', min_value=-1, max_value=1, step=1, value=0)
+        feature22 = st.slider('Gas-Agent (steam/none/CO2 —— -1/0/1)', min_value=-1, max_value=1, step=1, value=0)
         feature23 = st.slider('Gas-Flow (mL/min)', min_value=0.0, max_value=100.0, step=0.1, value=0.0)
         feature24 = st.slider('Gas-AT (°C)', min_value=0, max_value=900, step=10, value=0)
         feature25 = st.slider('Gas-At (min)', min_value=0, max_value=180, step=1, value=0)
 
 with col6:
     st.markdown('<p style="font-size: 24px; color: red; font-weight: bold; font-family: \'Times New Roman\', Times, serif; ">Adsorption</p>', unsafe_allow_html=True)
-    feature31 = st.slider('CO<sub>2</sub>Ad-T (°C)', min_value=0, max_value=75, step=25, value=0)
-    feature32 = st.slider('CO<sub>2</sub>Ad-P (bar)', min_value=0.1, max_value=1.0, step=0.1, value=1.0)
+    feature31 = st.slider('CO2Ad-T (°C)', min_value=0, max_value=75, step=25, value=0)
+    feature32 = st.slider('CO2Ad-P (bar)', min_value=0.1, max_value=1.0, step=0.1, value=1.0)
 
 # 预测按钮
 if st.button('Predict'):
