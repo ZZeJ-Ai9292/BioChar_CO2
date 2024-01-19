5531d1f5469e62321e71f6e744470a66491779ab
Andirod9262 <155721755+Andirod9262@users.noreply.github.com>
2024-01-19 14:19:18 +0800

    Update App.py

--- App.py
+++ App.py
@@ -27,6 +27,7 @@ with col2:
     feature28 = st.slider('HR (°C/min)', min_value=0, max_value=50, step=1, value=3)
     feature29 = st.slider('PT (°C)', min_value=0, max_value=1000, step=1, value=440)
     feature30 = st.slider('Pt (min)', min_value=0, max_value=240, step=1, value=50)
+    st.markdown('<p style="font-size: 24px; color: red; font-weight: bold; font-family: \'Times New Roman\', Times, serif; "> </p>', unsafe_allow_html=True)
     st.markdown('<p style="font-size: 24px; color: red; font-weight: bold; font-family: \'Times New Roman\', Times, serif; ">Adsorption</p>', unsafe_allow_html=True)
     feature31 = st.slider('CO2Ad-T (°C)', min_value=0, max_value=75, step=25, value=0)
     feature32 = st.slider('CO2Ad-P (bar)', min_value=0.1, max_value=1.0, step=0.1, value=1.0)
